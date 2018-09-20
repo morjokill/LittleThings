6ac26ec4200f8aa89d2f233bbbc4ce5b7e1df1e9
romanovmv <romanov1maxim@gmail.com>
2018-09-20 13:55:20 +0300

    add task3

--- src/main/java/algorithm/task3/Task3.java
+++ src/main/java/algorithm/task3/Task3.java
@@ -1,7 +1,28 @@
-package algorithm.task3;
+import java.util.Random;
 
 public class Task3 {
     public static void main(String[] args) {
+        int n = 5;
+        int accuracy = 1000000;
+        int count = 0;
+        for (int i = 0; i < accuracy; i++) {
+            double x = getRandomDouble();
+            double up = x + 1;
+            double sum = 0;
+            for (int j = 0; j < n - 1; j++) {
+                sum += getRandomDouble();
+            }
+            double down = sum + n  + x;
+            if (up / down > getRandomDouble()) {
+                count++;
+            }
+        }
+        double P = (double) count / accuracy;
+        System.out.println("Accuracy: " + P);
+    }
 
+    private static double getRandomDouble() {
+        Random rd = new Random();
+        return rd.nextDouble();
     }
 }
