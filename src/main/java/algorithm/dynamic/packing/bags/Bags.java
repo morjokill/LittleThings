d1fe93decd709f1d1fb1788ecb21257ce797ae40
morjokill <morjokill@gmail.com>
2018-12-05 20:42:29 +0300

    add bags

--- src/main/java/algorithm/dynamic/packing/bags/Bags.java
+++ src/main/java/algorithm/dynamic/packing/bags/Bags.java
@@ -1,12 +1,11 @@
 package algorithm.dynamic.packing.bags;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Bags {
     public static void main(String[] args) {
         int[] input = new int[]{
-                21,
+                31,
                 3, 20,
                 5, 32,
                 6, 2,
@@ -15,16 +14,37 @@ public class Bags {
                 10, 8,
         };
 
-        int m = input[0];
-        List<Item> items = new LinkedList<>();
-        int[] arrayF = new int[m + 1];
         if (input.length % 2 != 0) {
-            for (int i = 1; i < input.length; i += 2) {
-                items.add(new Item(input[i], input[i + 1]));
+            int weightCup = input[0];
+            int[] weights = new int[(input.length - 1) / 2 + 1];
+            int[] costs = new int[(input.length - 1) / 2 + 1];
+            int k = 1;
+            for (int i = 1; i < input.length - 1; i += 2) {
+                weights[k] = input[i];
+                costs[k] = input[i + 1];
+                k++;
             }
-            System.out.println(items);
+            System.out.println(Arrays.toString(weights));
+            System.out.println(Arrays.toString(costs));
+
+            System.out.println(calcBackPack(weights, costs, weightCup));
         } else {
             System.out.println("Corrupted input");
         }
+    }
+
+    private static int calcBackPack(int weights[], int costs[], int weightCup) {
+        int weightsLength = weights.length;
+        int arrayF[][] = new int[weightCup + 1][weightsLength + 1];
+        for (int j = 1; j <= weightsLength; j++) {
+            for (int w = 1; w <= weightCup; w++) {
+                if (weights[j - 1] <= w) {
+                    arrayF[w][j] = Math.max(arrayF[w][j - 1], arrayF[w - weights[j - 1]][j - 1] + costs[j - 1]);
+                } else {
+                    arrayF[w][j] = arrayF[w][j - 1];
+                }
+            }
+        }
+        return arrayF[weightCup][weightsLength];
     }
 }
