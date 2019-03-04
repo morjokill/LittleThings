dc8a8b6ea27b4931c221c068cf58355c287972a7
morjokill <morjokill@gmail.com>
2019-03-04 10:47:05 +0300

    Add search by words

---
+++ src/main/java/search/Article.java
@@ -0,0 +1,50 @@
+package search;
+
+import java.util.Objects;
+
+public class Article {
+    private String id;
+    private String url;
+
+    public Article(String id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(id, article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id='" + id + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
--- src/main/resources/search_table.sql
+++ src/main/resources/search_table.sql
@@ -18,3 +18,24 @@ id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 term VARCHAR(64),
 articles_id UUID REFERENCES articles (id)
 );
+
+CREATE TABLE terms_list (
+  term_id   UUID REFERENCES words_mystem (id) PRIMARY KEY,
+  term_text VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE article_term (
+  article_id UUID,
+  term_id    UUID REFERENCES terms_list (term_id)
+);
+
+
+INSERT INTO terms_list SELECT DISTINCT ON (term)
+                         id   term_id,
+                         term term_text
+                       FROM words_mystem;
+
+INSERT INTO article_term SELECT
+  m2.articles_id article_id,
+  tl.term_id term_id
+FROM terms_list tl INNER JOIN words_mystem m2 on tl.term_text = m2.term;
