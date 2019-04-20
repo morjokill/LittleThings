0ef4411402e0b4ed14e066020612ed571873f392
morjokill <morjokill@gmail.com>
2019-04-20 15:44:42 +0300

    Add asList example

---
+++ actual_little_things/src/main/java/little/things/arrays/AsList.java
@@ -0,0 +1,11 @@
+package little.things.arrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AsList {
+    public static void main(String[] args) {
+        List<String> lineList = Arrays.asList("first", "second");
+        System.out.println(lineList);
+    }
+}
