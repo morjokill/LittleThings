c097eef390e44904cff8a4038ec7ff92e5143cb5
romanovmv <morjokill@gmail.com>
2019-05-27 15:58:46 +0300

    Add health check to dropwizard example

--- dropwizard/src/main/java/app/Application.java
+++ dropwizard/src/main/java/app/Application.java
@@ -1,17 +1,18 @@
 package app;
 
+import app.health_check.TestHealthCheck;
 import controller.SimpleController;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
 
 public class Application extends io.dropwizard.Application<Configuration> {
     public static void main(String[] args) throws Exception {
-        //run with args: {server}
-        new Application().run(args);
+        new Application().run("server");
     }
 
     @Override
     public void run(Configuration configuration, Environment environment) {
         environment.jersey().register(new SimpleController());
+        environment.healthChecks().register("test_heath_check", new TestHealthCheck());
     }
 }
---
+++ dropwizard/src/main/java/app/health_check/TestHealthCheck.java
@@ -0,0 +1,10 @@
+package app.health_check;
+
+import com.codahale.metrics.health.HealthCheck;
+
+public class TestHealthCheck extends HealthCheck {
+    @Override
+    protected Result check() throws Exception {
+        return Result.healthy("All is gucci!");
+    }
+}
