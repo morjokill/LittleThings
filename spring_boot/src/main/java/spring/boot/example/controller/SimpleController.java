87e939dfb4cbdd7fab452a948eef00ba2adb0871
Romanov_M <m.romanov@bpcbt.com>
2020-08-04 15:59:54 +0300

    Add file download endpoint

--- spring_boot/src/main/java/spring/boot/example/controller/SimpleController.java
+++ spring_boot/src/main/java/spring/boot/example/controller/SimpleController.java
@@ -1,8 +1,17 @@
 package spring.boot.example.controller;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/")
@@ -11,5 +20,26 @@ public class SimpleController {
     @GetMapping("/hi")
     public String hi() {
         return "Русские вперед!";
+    }
+
+    @RequestMapping(path = "/download", method = RequestMethod.GET)
+    public ResponseEntity<Resource> download() {
+        String transactionId = "123910";
+        String line = "wow";
+        String fileName = "transaction" + transactionId + ".log";
+        InputStreamResource resource = new InputStreamResource(
+                new ByteArrayInputStream(line.getBytes(StandardCharsets.UTF_8)));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(line.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 }
